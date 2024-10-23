786d1efc4f82871c65661ab1585422f91eb0b7df
Joseph Lloyd <joseph.lloyd17@hotmail.com>
2024-10-23 16:49:50 +0100

    some changes

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<div class="pure-menu pure-menu-horizontal">
+  <a href="#" class="pure-menu-heading pure-menu-link">DREAMSPACE</a>
+  <ul class="pure-menu-list">
+      <li class="pure-menu-item">
+          <a href="#" class="pure-menu-link">News</a>
+      </li>
+      <li class="pure-menu-item">
+          <a href="#" class="pure-menu-link">Sports</a>
+      </li>
+      <li class="pure-menu-item">
+          <a href="#" class="pure-menu-link">Finance</a>
+      </li>
+  </ul>
+</div>
+
+<style>
+  .pure-menu {
+    text-align: center;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Navbar from "$lib/components/Navbar.svelte";
+  import "../global.css";
+  import "purecss";
+</script>
+
+<Navbar />
+<main>
+  <slot />
+</main>
